9183632e3266e83b94635fa5d126d18e1f706645
hyunjong_lee <hyunjong.lee.s@gmail.com>
2016-01-30 12:07:44 +0900

    QLearning 구현 중

--- ReinforcementLearning/core/GameState.cs
+++ ReinforcementLearning/core/GameState.cs
@@ -11,12 +11,23 @@ namespace core
         public int UserHp { get; set; }
         public int UserPos { get; set; }
         public int TowerHp { get; set; }
+
+        public static readonly int USER_POS_MIN = 0;
+        public static readonly int USER_POS_MAX = 2;
+
+        public IEnumerable<Action> GetActionSet()
+        {
+            yield return Action.STOP;
+            if (UserPos > USER_POS_MIN) yield return Action.LEFT;
+            if (UserPos < USER_POS_MAX) yield return Action.RIGHT;
+        }
     }
 
     public enum Action
     {
-        WEST,
-        EAST,
+        LEFT,
+        RIGHT,
         STOP,
+        NONE,
     }
 }
--- ReinforcementLearning/core/QLearningAgent.cs
+++ ReinforcementLearning/core/QLearningAgent.cs
@@ -8,10 +8,15 @@ namespace core
 {
     public class QLearningAgent
     {
+        private Dictionary<Tuple<GameState, Action>, Double> _stateQValueMap;
+
         private double _epsilon;
         private double _gamma;
         private double _alpha;
         private int _numberOfTraining;
+
+        private static double ACTION_MIN_VALUE = -987654321.0;
+
 
         public QLearningAgent()
         {
@@ -19,36 +24,80 @@ namespace core
 
         double getQValue(GameState state, Action action)
         {
-            return 0.0;
+            return _stateQValueMap[Tuple.Create(state, action)];
         }
 
         double computeValueFromQValues(GameState state)
         {
-            return 0.0;
+            Action action = Action.NONE;
+            var value = ACTION_MIN_VALUE;
+
+            var availActions = state.GetActionSet();
+            foreach (var a in availActions)
+            {
+                var v = _stateQValueMap[Tuple.Create(state, a)];
+                if (v > value)
+                {
+                    action = a;
+                    value = v;
+                }
+            }
+
+            return value;
         }
 
         Action computeActionFromQValues(GameState state)
         {
-            return Action.STOP;
+            Action action = Action.NONE;
+            var value = ACTION_MIN_VALUE;
+
+            var availActions = state.GetActionSet();
+            foreach (var a in availActions)
+            {
+                var v = _stateQValueMap[Tuple.Create(state, a)];
+                if (v > value)
+                {
+                    action = a;
+                    value = v;
+                }
+            }
+
+            return action;
         }
 
         Action getAction(GameState state)
         {
-            return Action.STOP;
+            var availActions = state.GetActionSet();
+            Random rand = new Random();
+            var flipCoin = rand.NextDouble();
+            if (flipCoin < _epsilon)
+            {
+                var selected = rand.Next(availActions.Count());
+                return availActions.Skip(selected - 1).Take(1).First();
+            }
+            else
+            {
+                return computeActionFromQValues(state);
+            }
         }
 
         void update(GameState state, Action action, GameState nextState, double reward)
         {
+            var qValue = 
+                (1 - _alpha) * getQValue(state, action) +
+                _alpha * (reward + _epsilon * computeValueFromQValues(nextState));
+
+            _stateQValueMap[Tuple.Create(state, action)] = qValue;
         }
 
         Action getPolicy(GameState state)
         {
-            return Action.STOP;
+            return computeActionFromQValues(state);
         }
 
         double getValue(GameState state)
         {
-            return 0.0;
+            return computeValueFromQValues(state);
         }
     }
 }
